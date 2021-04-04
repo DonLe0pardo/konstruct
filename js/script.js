7e0dfadd90ad2adaaa828efded3db0532399dec6
DonLe0pardo <christinatemnikova@gmail.com>
2021-04-05 03:27:46 +0500

    comit 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 const burger = document.querySelector('.burger');
-const menu = document.querySelector(".header-mobile");
+const menu = document.querySelector('.header-mobile');
 
 burger.addEventListener('click', () => {
     menu.classList.toggle('header-mobile_active');
     burger.classList.toggle('burger_active');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
